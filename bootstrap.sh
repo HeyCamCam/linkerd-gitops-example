b6edd3fde570b61a2640197449cad8ae43888c67
Cam <2419734-HeyCamCam@users.noreply.gitlab.com>
2026-09-10 15:07:15 -0400

    check for cli tools before deploying

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,71 +2,156 @@
 
 set -e
 
+tools_dir="./tools"
 server_ip="192.168.0.2"
 remote_cluster_names=("k3d-cluster-a" "k3d-cluster-b")
-argocd_version="v3.5.2"
 argocd_port="8080"
+
+tools_check() {
+  if [ ! -d "$tools_dir" ]; then
+    mkdir -p $tools_dir
+  fi
+
+  echo -e "Checking if cli tools are installed.\n"
+  check_for_kubectl
+  check_for_argocd
+  check_for_k3d
+  check_for_linkerd
+  check_for_kubeseal
+  check_for_step
+
+  echo -e "All needed cli binaries are present.\n"
+}
+
+check_for_kubectl() {
+  if command -v $tools_dir/kubectl &>/dev/null; then
+    echo "Found Kubectl"
+  else
+    echo -e "Kubectl not found. Downloading."
+    curl -sL -o $tools_dir/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x $tools_dir/kubectl
+    echo -e "Download of Kubectl is complete.\n"
+  fi
+}
+
+check_for_argocd() {
+  if command -v $tools_dir/argocd version &>/dev/null; then
+    echo "Found ArgoCD"
+  else
+    echo "ArgoCD not found. Downloading."
+    curl -sL -o $tools_dir/argocd https://github.com/argoproj/argo-cd/releases/download/v3.5.2/argocd-linux-amd64
+    chmod +x $tools_dir/argocd
+    echo -e "Download of ArgoCD is complete.\n"
+  fi
+}
+
+check_for_k3d() {
+  if command -v $tools_dir/k3d version &>/dev/null; then
+    echo "Found K3D"
+  else
+    echo "K3D not found. Downloading."
+    curl -sL -o $tools_dir/k3d https://github.com/k3d-io/k3d/releases/download/v5.9.0/k3d-linux-amd64
+    chmod +x $tools_dir/k3d
+    echo -e "Download of K3D is complete.\n"
+  fi
+}
+
+check_for_linkerd() {
+  if command -v $tools_dir/linkerd version &>/dev/null; then
+    echo "Found Linkerd"
+  else
+    echo "Linkerd not found. Downloading."
+    curl -sL -o $tools_dir/linkerd https://github.com/linkerd/linkerd2/releases/download/edge-26.9.1/linkerd2-cli-edge-26.9.1-linux-amd64
+    chmod +x $tools_dir/linkerd
+    echo -e "Download of Linkerd is complete.\n"
+  fi
+}
+
+check_for_kubeseal() {
+  if command -v $tools_dir/kubeseal --version &>/dev/null; then
+    echo "Found Kubeseal"
+  else
+    echo "Kubeseal not found. Downloading."
+    curl -sL -o $tools_dir/kubeseal.tar.gz https://github.com/bitnami/sealed-secrets/releases/download/v0.40.0/kubeseal-0.40.0-linux-amd64.tar.gz
+    tar -xvf $tools_dir/kubeseal.tar.gz -C tools/
+    rm $tools_dir/{kubeseal.tar.gz,LICENSE,README.md}
+    echo -e "Download of Kubeseal is complete.\n"
+  fi
+}
+
+check_for_step() {
+  if command -v $tools_dir/step version &>/dev/null; then
+    echo "Found Step"
+  else
+    echo "Step cli not found. Downloading."
+    curl -sL -o $tools_dir/step_linux_amd64.tar.gz https://dl.smallstep.com/cli/docs-cli-install/latest/step_linux_amd64.tar.gz
+    tar -xvf $tools_dir/step_linux_amd64.tar.gz -C $tools_dir/ step_linux_amd64/bin/step
+    mv $tools_dir/step_linux_amd64/bin/step $tools_dir/step
+    rm -rf $tools_dir/step_linux_amd64.tar.gz $tools_dir/step_linux_amd64
+    echo -e "Download of Step is complete.\n"
+  fi
+}
 
 deploy_clusters() {
   echo -e "\nCreating k3d-gitops-network docker network"
   docker network create k3d-gitops-network
 
   echo -e "\nCreating k3d-management cluster"
-  k3d cluster create management --network k3d-gitops-network --api-port ${server_ip}:6445 --k3s-arg "--tls-san=${server_ip}@server:0" --port "${server_ip}:81:80@loadbalancer"
+  $tools_dir/k3d cluster create management --network k3d-gitops-network --api-port ${server_ip}:6445 --k3s-arg "--tls-san=${server_ip}@server:0" --port "${server_ip}:81:80@loadbalancer"
 
   echo -e "\nCreating k3d-cluster-a"
-  k3d cluster create cluster-a --network k3d-gitops-network --api-port ${server_ip}:6446 --k3s-arg "--tls-san=${server_ip}@server:0" --port "${server_ip}:82:80@loadbalancer"
+  $tools_dir/k3d cluster create cluster-a --network k3d-gitops-network --api-port ${server_ip}:6446 --k3s-arg "--tls-san=${server_ip}@server:0" --port "${server_ip}:82:80@loadbalancer"
 
   echo -e "\nCreating k3d-cluster-b"
-  k3d cluster create cluster-b --network k3d-gitops-network --api-port ${server_ip}:6447 --k3s-arg "--tls-san=${server_ip}@server:0" --port "${server_ip}:83:80@loadbalancer"
+  $tools_dir/k3d cluster create cluster-b --network k3d-gitops-network --api-port ${server_ip}:6447 --k3s-arg "--tls-san=${server_ip}@server:0" --port "${server_ip}:83:80@loadbalancer"
 
-  kubectl config use-context k3d-management
+  $tools_dir/kubectl config use-context k3d-management
 }
 
 install_argocd() {
-  kubectl config use-context k3d-management
+  $tools_dir/kubectl config use-context k3d-management
 
   echo -e "\nInstalling ArgoCD"
-  kubectl create namespace argocd
-  kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/${argocd_version}/manifests/install.yaml
-  
+  $tools_dir/kubectl create namespace argocd
+  $tools_dir/kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.5.2/manifests/install.yaml
+
   echo -e "\nWaiting for all ArgoCD pods to become ready"
-  kubectl wait --for=condition=ready pod --all -n argocd --timeout=300s
+  $tools_dir/kubectl wait --for=condition=ready pod --all -n argocd --timeout=300s
 }
 
 start_port_forward() {
-  kubectl -n argocd port-forward svc/argocd-server --address 0.0.0.0 ${argocd_port}:443 >/dev/null 2>&1 &
+  $tools_dir/kubectl -n argocd port-forward svc/argocd-server --address 0.0.0.0 ${argocd_port}:443 >/dev/null 2>&1 &
   PF_PID=$!
   sleep 2
 }
 
 show_argocd_password() {
   echo "Please open a web browser and navigate to https://${server_ip}:${argocd_port} to login."
-  echo -e "\nArgoCD Login\nUsername: admin\nPassword: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+  echo -e "\nArgoCD Login\nUsername: admin\nPassword: $($tools_dir/kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
   echo "Waiting 20 seconds before continuing"
   sleep 20
 }
 
 login_to_argocd() {
-  kubectl config use-context k3d-management
-  admin_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-  argocd login localhost:${argocd_port} --insecure --username admin --password $admin_password
+  $tools_dir/kubectl config use-context k3d-management
+  admin_password=$(${tools_dir}/kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+  $tools_dir/argocd login localhost:${argocd_port} --insecure --username admin --password $admin_password
 }
 
 generate_linkerd_certificates() {
   echo "Creating the Linkerd trust anchor certificate"
   mkdir -p pki/trust-anchor
-  step certificate create root.linkerd.cluster.local pki/trust-anchor/trust-anchor-ca.crt pki/trust-anchor/trust-anchor-ca.key --profile root-ca --no-password --insecure --not-after 43800h
+  $tools_dir/step certificate create root.linkerd.cluster.local pki/trust-anchor/trust-anchor-ca.crt pki/trust-anchor/trust-anchor-ca.key --profile root-ca --no-password --insecure --not-after 43800h
 
   echo "Generating the Linkerd intermediate certificate and key pair that will be used to sign the Linkerd proxies CSR"
   mkdir -p pki/intermediate
-  step certificate create identity.linkerd.cluster.local pki/intermediate/issuer.crt pki/intermediate/issuer.key --profile intermediate-ca --not-after 8760h --no-password --insecure --ca pki/trust-anchor/trust-anchor-ca.crt --ca-key pki/trust-anchor/trust-anchor-ca.key
+  $tools_dir/step certificate create identity.linkerd.cluster.local pki/intermediate/issuer.crt pki/intermediate/issuer.key --profile intermediate-ca --not-after 8760h --no-password --insecure --ca pki/trust-anchor/trust-anchor-ca.crt --ca-key pki/trust-anchor/trust-anchor-ca.key
 }
 
 create_sa_token() {
   for remote_cluster in "${remote_cluster_names[@]}"; do
-    kubectl config use-context ${remote_cluster}
-    kubectl apply -f - <<EOF
+    $tools_dir/kubectl config use-context ${remote_cluster}
+    $tools_dir/kubectl apply -f - <<EOF
 apiVersion: v1
 kind: ServiceAccount
 metadata:
@@ -99,68 +184,69 @@ type: kubernetes.io/service-account-token
 EOF
   done
 
-  kubectl config use-context k3d-management
+  $tools_dir/kubectl config use-context k3d-management
 }
 
 add_remote_clusters_to_argocd() {
-  kubectl config use-context k3d-management
+  $tools_dir/kubectl config use-context k3d-management
 
   for remote_cluster in "${remote_cluster_names[@]}"; do
-    argocd cluster add ${remote_cluster} --service-account argocd-manager --label linkerd=enabled --yes
+    $tools_dir/argocd cluster add ${remote_cluster} --service-account argocd-manager --label linkerd=enabled --yes
   done
 }
 
 deploy_prerequisite_appsets() {
-  kubectl config use-context k3d-management
+  $tools_dir/kubectl config use-context k3d-management
 
-  kubectl apply -f applicationsets/gateway-api
-  kubectl apply -f applicationsets/sealed-secrets
-  kubectl apply -f applicationsets/cert-manager
-  kubectl apply -f applicationsets/trust-manager
- 
+  $tools_dir/kubectl apply -f applicationsets/gateway-api
+  $tools_dir/kubectl apply -f applicationsets/sealed-secrets
+  $tools_dir/kubectl apply -f applicationsets/cert-manager
+  $tools_dir/kubectl apply -f applicationsets/trust-manager
+
   echo -e "\nWaiting for the prerequisite applications to become ready"
-  argocd app wait -l '!app.kubernetes.io/instance' --timeout 600 > /dev/null 2>&1
+  $tools_dir/argocd app wait -l '!app.kubernetes.io/instance' --timeout 600 >/dev/null 2>&1
 }
 
 configure_sealed_secrets() {
   for remote_cluster in "${remote_cluster_names[@]}"; do
-    kubectl config use-context ${remote_cluster}
+    $tools_dir/kubectl config use-context ${remote_cluster}
 
     echo "Retrieving the sealed-secrets controller public key"
     mkdir -p pki/trust-anchor/${remote_cluster}
 
-    kubeseal --controller-name sealed-secrets --fetch-cert >pki/trust-anchor/${remote_cluster}/${remote_cluster}-sealed-secrets-pub-cert.pem
+    $tools_dir/kubeseal --controller-name sealed-secrets --fetch-cert >pki/trust-anchor/${remote_cluster}/${remote_cluster}-sealed-secrets-pub-cert.pem
 
     echo "Sealing the Linkerd trust anchor as a kubernetes tls secret for ${remote_cluster}"
-    kubectl create secret tls linkerd-trust-anchor \
+    $tools_dir/kubectl create secret tls linkerd-trust-anchor \
       --cert=pki/trust-anchor/trust-anchor-ca.crt \
       --key=pki/trust-anchor/trust-anchor-ca.key \
       --namespace=cert-manager \
       --dry-run=client -o yaml |
-      kubeseal --cert pki/trust-anchor/${remote_cluster}/${remote_cluster}-sealed-secrets-pub-cert.pem \
+      $tools_dir/kubeseal --cert pki/trust-anchor/${remote_cluster}/${remote_cluster}-sealed-secrets-pub-cert.pem \
         --format yaml \
         --controller-name=sealed-secrets \
         --controller-namespace=kube-system |
-      kubectl patch -f - \
+      $tools_dir/kubectl patch -f - \
         --type=merge \
         --local -o yaml \
         -p '{"spec":{"template":{"metadata":{"labels":{"linkerd":"enabled"}}}}}' \
         >pki/trust-anchor/${remote_cluster}/sealed-linkerd-trust-anchor-certificate.yml
-  
-    kubectl apply -f pki/trust-anchor/${remote_cluster}/sealed-linkerd-trust-anchor-certificate.yml
+
+    $tools_dir/kubectl apply -f pki/trust-anchor/${remote_cluster}/sealed-linkerd-trust-anchor-certificate.yml
   done
-  
-  kubectl config use-context k3d-management
+
+  $tools_dir/kubectl config use-context k3d-management
 }
 
 deploy_linkerd() {
-  kubectl config use-context k3d-management
-  kubectl apply -f management-cluster/management-cluster.yml
+  $tools_dir/kubectl config use-context k3d-management
+  $tools_dir/kubectl apply -f management-cluster/management-cluster.yml
 
   echo -e "\nLinkerd is now being deployed"
 }
 
 deploy_linkerd_gitops_example() {
+  tools_check
   deploy_clusters
   install_argocd
   start_port_forward
@@ -175,9 +261,9 @@ deploy_linkerd_gitops_example() {
 }
 
 delete_linkerd_gitops_example() {
-  k3d cluster delete management
-  k3d cluster delete cluster-a
-  k3d cluster delete cluster-b
+  $tools_dir/k3d cluster delete management
+  $tools_dir/k3d cluster delete cluster-a
+  $tools_dir/k3d cluster delete cluster-b
   docker network rm k3d-gitops-network
   rm -rf ./pki/intermediate
   rm -rf ./pki/trust-anchor
